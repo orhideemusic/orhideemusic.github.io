aa63939ababa7dfdc89feebe13867aa0e5cc7d38
orhideemusic <89600196+orhideemusic@users.noreply.github.com>
2021-09-09 14:32:41 +0200

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -32,4 +32,24 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
-// tictacktoe
+
+
+window.onload = function() {
+    (function() {
+        var visited = localStorage.getItem('visited');
+        if (!visited) {
+            document.getElementById("third").style.visibility = "visible";
+            localStorage.setItem('visited', true);
+        }
+    })();
+} //]]>
+
+const targetDiv = document.getElementById("third");
+const btn = document.getElementById("toggle");
+btn.onclick = function() {
+    if (targetDiv.style.display !== "none") {
+        targetDiv.style.display = "none";
+    } else {
+        targetDiv.style.display = "block";
+    }
+};
